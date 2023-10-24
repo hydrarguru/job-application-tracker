32c3b3306a1e5aa3c5de1ef102532678aad78733
hydrarguru <henrik@engqvist.org>
2023-10-24 10:51:39 +0200

    chore: added webpack build config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+   mode: "production",
+   entry: {
+      background: path.resolve(__dirname, "..", "src/", "background.ts"),
+      content: path.resolve(__dirname, "..", "src/", "content.ts"),
+      popup: path.resolve(__dirname, "..", "src/", "popup.ts"),
+      options: path.resolve(__dirname, "..", "src/", "options.ts"),
+   },
+   output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "[name].js",
+   },
+   resolve: {
+      extensions: [".ts", ".js"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   plugins: [
+      new CopyPlugin({
+         patterns: [
+         { from: "public", to: "." },
+         { from: "src/options", to: "." },
+         { from: "src/popup", to: "." },
+      ],
+      }),
+   ],
+};
