14b7ba1669d4d524effa74baeb7d52348ee656d3
hydrarguru <henrik@engqvist.org>
2023-10-24 13:05:05 +0200

    Update webpack.config.js
    formatting using prettier.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,36 +1,41 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
-   mode: "production",
-   entry: {
-      background: path.resolve(__dirname, "..", "src/", "background.ts"),
-      content: path.resolve(__dirname, "..", "src/", "content.ts"),
-      popup: path.resolve(__dirname, "..", "src/", "popup.ts"),
-      options: path.resolve(__dirname, "..", "src/", "options.ts"),
-   },
-   output: {
-      path: path.join(__dirname, "../dist"),
-      filename: "[name].js",
-   },
-   resolve: {
-      extensions: [".ts", ".js"],
-   },
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-         },
-      ],
-   },
-   plugins: [
-      new CopyPlugin({
-         patterns: [
-         { from: "public", to: "." },
-         { from: "src/options", to: "." },
-         { from: "src/popup", to: "." },
-      ],
-      }),
-   ],
+    mode: 'production',
+    entry: {
+        background: path.resolve(
+            __dirname,
+            '..',
+            'src/scripts',
+            'background.ts'
+        ),
+        content: path.resolve(__dirname, '..', 'src/scripts', 'content.ts'),
+        popup: path.resolve(__dirname, '..', 'src/scripts', 'popup.ts'),
+        options: path.resolve(__dirname, '..', 'src/scripts', 'options.ts'),
+    },
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'public', to: '.' },
+                { from: 'src/options', to: '.' },
+                { from: 'src/popup', to: '.' },
+            ],
+        }),
+    ],
 };
