aec551ce7c6c54b69571cf3e44d3abf9cccf300a
hydrarguru <henrik@engqvist.org>
2023-10-28 17:45:26 +0200

    feat: Add data to database function

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,1 +1,14 @@
-console.log('background.ts/js');
+import { createDatabase, addJobToDatabase } from './db';
+import { getCurrentMonth } from './utils';
+
+createDatabase();
+
+/* 
+addJobToDatabase('october', {
+    companyName: 'Test Företag',
+    jobRole: 'Test Roll',
+    jobArea: 'Test Ort',
+    applicationLink: 'https://www.test.se',
+    applicationDate: '2021-01-01',
+});
+*/
--- src/scripts/db.ts
+++ src/scripts/db.ts
@@ -15,15 +15,31 @@ const months = [
     'december',
 ];
 
+type JobData = {
+    companyName: string;
+    jobRole: string;
+    jobArea: string;
+    applicationLink: string;
+    applicationDate: string;
+};
+
 export function createDatabase() {
     openDB('JAT-DB', 1, {
         upgrade(db) {
             for (const month of months) {
-                db.createObjectStore(`${month}-jobs`, {
+                db.createObjectStore(`${month}`, {
                     keyPath: 'id',
                     autoIncrement: true,
                 });
             }
         },
+    });
+}
+
+export function addJobToDatabase(month: string, job: JobData) {
+    openDB('JAT-DB', 1).then((db) => {
+        const transaction = db.transaction(`${month}`, 'readwrite');
+        const store = transaction.objectStore(`${month}`);
+        store.add(job);
     });
 }
