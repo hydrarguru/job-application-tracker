ef9fc242eb93b85ac6a401d665d9fe4b3f40e998
hydrarguru <henrik@engqvist.org>
2023-11-04 01:56:43 +0100

    feat(db): simple function to create dummy db data.

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,5 +1,25 @@
-import { createDatabase, getJobsFromDatabase } from './db';
+import { addJobToDatabase, createDatabase, getJobsFromDatabase } from './db';
 createDatabase();
+
+for (let i = 1; i < 6; i++) {
+    addJobToDatabase({
+        companyName: `Test Company ${i}`,
+        jobRole: `Test Role ${i}`,
+        jobArea: `Test Area ${i}`,
+        applicationLink: `Test Link ${i}`,
+        appliedDate: `Test Date ${i}`,
+    });
+}
+
+/*
+addJobToDatabase({
+    companyName: 'Test Company 1',
+    jobRole: 'Test Role 1',
+    jobArea: 'Test Area 1',
+    applicationLink: 'Test Link 1',
+    appliedDate: 'Test Date 1',
+});
+*/
 
 async function createOffscreen() {
     if (await chrome.offscreen.hasDocument()) return;
@@ -9,7 +29,7 @@ async function createOffscreen() {
       justification: "storing data in local storage",
     });
     const date = new Date();
-    console.log(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] Created offscreen document.`)
+    console.info(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] Created offscreen document.`)
 }
   
 chrome.runtime.onMessage.addListener(async (msg) => {
