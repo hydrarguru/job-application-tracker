bb451a4cc977261464d5802d0eda83c1045af1d7
hydrarguru <henrik@engqvist.org>
2023-11-05 03:10:28 +0100

    Update offscreen.ts

--- src/scripts/offscreen.ts
+++ src/scripts/offscreen.ts
@@ -1,13 +1,16 @@
 import { getJobsFromDatabase } from "./db";
 
+function setLocalStorageValues(total, day, month) {
+    localStorage.setItem("totalJobs", total);
+    localStorage.setItem("totalJobsDay", day);
+    localStorage.setItem("totalJobsMonth", month);
+    alert('localStorage values set!');
+}
+
 getJobsFromDatabase().then(
     (jobs) => {
         setLocalStorageValues(jobs.length, 0, 0);
     }
 );
 
-function setLocalStorageValues(total, day, month) {
-    localStorage.setItem("totalJobs", total);
-    localStorage.setItem("totalJobsDay", day);
-    localStorage.setItem("totalJobsMonth", month);
-}
+
