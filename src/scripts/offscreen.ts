7b9522d70d56958e94254171f9cb0aa32d5ac8c5
hydrarguru <henrik@engqvist.org>
2023-11-06 14:27:09 +0100

    feat: refresh localStorage values.

--- src/scripts/offscreen.ts
+++ src/scripts/offscreen.ts
@@ -7,24 +7,37 @@ async function getTotalJobsMonth(month: string): Promise<number> {
     const value = await db.getAllFromIndex(DATABASE_TABLE, 'appliedDate', IDBKeyRange.bound(`2023-${month}-01`, `2023-${month}-31`));
     return value.length;
 }
-//`2023-${month}-${day}`
+
 async function getTotalJobsToday(month: string, day: string): Promise<number> {
     const db = await openDB(DATABASE_NAME, 1);
     const value = await db.getAllFromIndex(DATABASE_TABLE, 'appliedDate', IDBKeyRange.bound(`2023-${month}-${day}`, `2023-${month}-${day}`));
     return value.length;
 }
 
-chrome.runtime.onMessage.addListener(async (msg) => {
-    if (msg === 'test') {
-        alert();
-    }
-});
+async function refreshBadge() {
+    chrome.action.setBadgeBackgroundColor({ color: '#294936' });
+    chrome.action.setBadgeTextColor({ color: '#ffffff' });
+    chrome.action.setBadgeText({text: '100' });
+}
 
-document.addEventListener('DOMContentLoaded', async () => {
+async function setLocalStorageValues() {
     localStorage.setItem('totalJobs', (await getJobsFromDatabase()).length.toString());
     localStorage.setItem('totalJobsDay', (await getTotalJobsToday('11', '06')).toString());
     localStorage.setItem('totalJobsMonth', (await getTotalJobsMonth('11')).toString());
     localStorage.setItem('currentMonth', getCurrentMonth());
+}
+
+chrome.runtime.onMessage.addListener(async (msg) => {
+    if (msg === 'refreshValues') {
+        await setLocalStorageValues();
+    }
+    if (msg === 'refreshBadge') {
+        await refreshBadge();
+    }
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await setLocalStorageValues();
 });
 
 
