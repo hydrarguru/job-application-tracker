065822a9086b7538d27f3928ba8ac7576abeb812
hydrarguru <henrik@engqvist.org>
2023-11-06 12:49:12 +0100

    refactor(offscreen): month & day filter for db.
    grabbing every job per month and on a given day works.

--- src/scripts/offscreen.ts
+++ src/scripts/offscreen.ts
@@ -1,30 +1,34 @@
 import { openDB } from "idb";
-import { getJobsFromDatabase } from "./db";
+import { getJobsFromDatabase, DATABASE_NAME, DATABASE_TABLE } from "./db";
 import { getCurrentMonth } from "./utils";
 
-function setLocalStorageValues(total, day, month) {
-    localStorage.setItem('totalJobs', total);
-    localStorage.setItem('totalJobsDay', day);
-    localStorage.setItem('totalJobsMonth', month);
-    localStorage.setItem('currentMonth', getCurrentMonth())
+async function getTotalJobsMonth(month: string): Promise<number> {
+    const db = await openDB(DATABASE_NAME, 1);
+    const value = await db.getAllFromIndex(DATABASE_TABLE, 'appliedDate', IDBKeyRange.bound(`2023-${month}-01`, `2023-${month}-31`));
+    return value.length;
+}
+//`2023-${month}-${day}`
+async function getTotalJobsToday(month: string, day: string): Promise<number> {
+    const db = await openDB(DATABASE_NAME, 1);
+    const value = await db.getAllFromIndex(DATABASE_TABLE, 'appliedDate', IDBKeyRange.bound(`2023-${month}-${day}`, `2023-${month}-${day}`));
+    return value.length;
 }
 
-function getTotalJobsMonth(month: string) {
-    const db = openDB('JAT-DB', 1).then(
-        (db) => {
-            const value = db.getAllFromIndex('jobsTable', 'appliedDate', IDBKeyRange.bound(`2023-${month}-01`, `2023-${month}-31`));
-            return value.then((value) => {
-                return value.length;
-            });
-        }
-    );
-}
-
-getJobsFromDatabase().then(
-    (jobs) => {
-        setLocalStorageValues(jobs.length, 0, getTotalJobsMonth('11'));
+chrome.runtime.onMessage.addListener(async (msg) => {
+    if (msg === 'test') {
+        alert();
     }
-);
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    localStorage.setItem('totalJobs', (await getJobsFromDatabase()).length.toString());
+    localStorage.setItem('totalJobsDay', (await getTotalJobsToday('11', '06')).toString());
+    localStorage.setItem('totalJobsMonth', (await getTotalJobsMonth('11')).toString());
+    localStorage.setItem('currentMonth', getCurrentMonth());
+});
+
+
+
 
 
 
