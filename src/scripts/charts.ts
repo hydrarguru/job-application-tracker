3542331159e27a88c0db9ebd6f77063d4b497c88
hydrarguru <henrik@engqvist.org>
2023-11-11 01:55:08 +0100

    feat(charts): charts now contain real data.

--- src/scripts/charts.ts
+++ src/scripts/charts.ts
@@ -1,12 +1,22 @@
 import Chart from 'chart.js/auto'
-import { getTotalJobsMonth } from './db';
+import { getTotalJobsMonth, indexKeyValueQuery } from './db';
+import { removeDuplicatesAndCount } from './utils';
 
-document.addEventListener('DOMContentLoaded', async () => {
-
-
-});
+document.addEventListener('DOMContentLoaded', async () => {});
 
 (async function() {
+  const pieChartColors = [
+    'rgba(246, 109, 68, 0.2)',
+    'rgba(254, 174, 101, 0.2)',
+    'rgba(170, 222, 167, 0.2)',
+    'rgba(45, 135, 187, 0.2)'
+  ];
+  const pieChartBorderColors = [
+    'rgba(246, 109, 68, 0.8)',
+    'rgba(254, 174, 101, 0.8)',
+    'rgba(170, 222, 167, 0.8)',
+    'rgba(45, 135, 187, 0.8)'
+  ];
   const monthsData = {
     january: Number(await getTotalJobsMonth('01')),
     february: Number(await getTotalJobsMonth('02')),
@@ -20,15 +30,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     october: Number(await getTotalJobsMonth('10')),
     november: Number(await getTotalJobsMonth('11')),
     december: Number(await getTotalJobsMonth('12'))
-  }
+  };
 
-
-  const totalJobMonthData = [
-    { month: 'Oktober', count: monthsData.october },
-    { month: 'November', count: monthsData.november },
-    { month: 'December', count: monthsData.december },
-  ];
-
+  //Total Jobs Year Chart(month: 1 -> 12)
   const totalJobsData = [
     { month: 'Januari', count: monthsData.january },
     { month: 'Februari', count: monthsData.february },
@@ -43,69 +47,183 @@ document.addEventListener('DOMContentLoaded', async () => {
     { month: 'November', count: monthsData.november },
     { month: 'December', count: monthsData.december }
   ];
-
   new Chart(
     document.getElementById('totalJobsCanvas') as HTMLCanvasElement,
     {
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            font: {
+              size: 20
+            },
+            display: true,
+            text: 'Totalt antal jobb under året'
+          }
+        }
+      },
       type: 'line',
       data: {
         labels: totalJobsData.map(row => row.month),
         datasets: [
           {
-            backgroundColor: 'rgba(40, 72, 53, 0.75)',
-            borderColor(ctx, options) {
-              return 'rgba(40, 72, 53, 1)';
-            },
-            label: `Totalt antal jobb under år ${new Date().getFullYear()}`,
+            backgroundColor: 'rgba(40, 72, 53, 0.25)',
+            borderColor: 'rgba(40, 72, 53, 1)',
+            fill: true,
+            label: `Antal jobb per månad`,
             data: totalJobsData.map(row => row.count)
           }
         ]
       }
     }
   );
-  
+
+  //Total Jobs Monthly Data Chart
+  const totalJobsMonthData = [
+    { month: 'Oktober', count: monthsData.october },
+    { month: 'November', count: monthsData.november },
+    { month: 'December', count: monthsData.december },
+  ];
   new Chart(
     document.getElementById('totalJobsMonthCanvas') as HTMLCanvasElement,
     {
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            font: {
+              size: 20
+            },
+            display: true,
+            text: 'Antal jobb'
+          },
+        }
+      },
       type: 'bar',
       data: {
-        labels: totalJobMonthData.map(row => row.month),
+        labels: totalJobsMonthData.map(row => row.month),
         datasets: [
           {
-            backgroundColor: 'rgba(40, 72, 53, 0.75)',
-            borderColor(ctx, options) {
-              return 'rgba(40, 72, 53, 1)';
-            },
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1,
             label: `Totalt antal jobb denna månad (${new Date().toLocaleString('sv-SE', { month: 'long' })})`,
-            data: totalJobMonthData.map(row => row.count)
-          }
+            data: [
+              monthsData.october,
+              monthsData.november,
+              monthsData.december
+            ]
+          },
         ]
       }
     }
   );
 
+  //Total Unique Companies Chart
+  const uniqueCompanyDataMap = removeDuplicatesAndCount(await indexKeyValueQuery('companyName'));
+  const uniqueCompanyData = Array.from(uniqueCompanyDataMap, ([company, count]) => ({ company, count }));
   new Chart(
-    document.getElementById('jobCategoryCanvas') as HTMLCanvasElement,
+    document.getElementById('CompanyCanvas') as HTMLCanvasElement,
     {
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'right',
+            labels: {
+            }
+          },
+          title: {
+            font: {
+              size: 20
+            },
+            display: true,
+            text: 'Unika företag'
+          },
+          subtitle: {
+            font: {
+              size: 14
+            },
+            display: true,
+            text: `${uniqueCompanyData.length} unika företag`
+          }
+        }
+      },
       type: 'pie',
       data: {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
+        labels: uniqueCompanyData.map(row => row.company),
         datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
+          label: `Företag`,
+          data: uniqueCompanyData.map(row => row.count),
+          backgroundColor: pieChartColors,
+          borderColor: pieChartBorderColors,
+          hoverOffset: 8,
         }]
       }
     }
   );
 
+  //Job Role Chart
+  const uniqueJobRolesDataMap = removeDuplicatesAndCount(await indexKeyValueQuery('jobRole'));
+  const uniqueJobRolesData = Array.from(uniqueJobRolesDataMap, ([role, count]) => ({ role, count }));
+  new Chart(
+    document.getElementById('jobRoleCanvas') as HTMLCanvasElement,
+    {
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'right',
+          },
+          title: {
+            font: {
+              size: 20
+            },
+            display: true,
+            text: 'Unika jobbroller'
+          },
+          subtitle: {
+            font: {
+              size: 14
+            },
+            display: true,
+            text: `${uniqueJobRolesData.length} unika jobbroller`
+          }
+        }
+      },
+      type: 'doughnut',
+      data: {
+        labels: uniqueJobRolesData.map(row => row.role),
+        datasets: [{
+          label: `Antal`,
+          data: uniqueJobRolesData.map(row => row.count),
+          backgroundColor: pieChartColors,
+          borderColor: pieChartBorderColors,
+          hoverOffset: 4
+        }]
+      }
+    }
+  );
 })();
