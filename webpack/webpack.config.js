6027e2b69f7665bf4fc457077162d71c1e4c4bd7
hydrarguru <henrik@engqvist.org>
2023-11-21 17:11:31 +0100

    refactor: removed content scripts.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
             'src/scripts',
             'background.ts'
         ),
-        content: path.resolve(__dirname, '..', 'src/scripts', 'content.ts'),
         popup: path.resolve(__dirname, '..', 'src/scripts', 'popup.ts'),
         dashboard: path.resolve(__dirname, '..', 'src/scripts', 'dashboard.ts'),
         offscreen: path.resolve(__dirname, '..', 'src/scripts', 'offscreen.ts'),
